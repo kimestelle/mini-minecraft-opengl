b813b57250d0ecb76cf1230ba92fcc89333f8d82
Estelle Kim <iamestellekim@gmail.com>
2024-12-09 13:14:16 -0500

    FIXED SKY???

--- assignment_package/glsl/sky.vert.glsl
+++ assignment_package/glsl/sky.vert.glsl
@@ -2,10 +2,8 @@
 
 in vec4 sky_Pos;
 out vec4 fs_Pos;
-uniform mat4 u_ViewProjInv;
 
 void main() {
-    //transform sky position to world space
-    fs_Pos = u_ViewProjInv * sky_Pos;
+    fs_Pos = sky_Pos;
     gl_Position = sky_Pos;
 }
